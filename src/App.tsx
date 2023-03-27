a3e9e2cb41a65aefb0bdf86ed6b0b3ba6bbb738e
Haoxin Yang <hxyang@alauda.io>
2023-03-27 14:41:37 +0800

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,4 @@
+import { ReactWidget } from '@jupyterlab/apputils';
+import App from './experiment-tracking/components/App';
+
+export default ReactWidget.create(<App></App>);
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,53 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import AppWidget from './App';
 
-/**
- * Initialization data for the myextension extension.
- */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'myextension:plugin',
+  id: 'jupyterlab-apod',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension myextension is activated!');
+  requires: [ICommandPalette],
+  activate: async (app: JupyterFrontEnd, palette: ICommandPalette) => {
+    console.log('JupyterLab extension jupyterlab_apod is activated!');
+    console.log(palette);
+
+    // Define a widget creator function,
+    // then call it to make a new widget
+    const newWidget = async () => {
+      // Create a blank content widget inside of a MainAreaWidget
+      const content = AppWidget;
+      const widget = new MainAreaWidget({ content });
+
+      widget.id = 'apod-jupyterlab';
+      widget.title.label = 'Astronomy Picture';
+      widget.title.closable = true;
+      return widget;
+    };
+    let widget = await newWidget();
+
+    // Add an application command
+    const command = 'apod:open';
+    app.commands.addCommand(command, {
+      label: 'Random Astronomy Picture',
+      execute: async () => {
+        // Regenerate the widget if disposed
+        if (widget.isDisposed) {
+          widget = await newWidget();
+        }
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({ command, category: 'Tutorial' });
   }
 };
 
